d50f5f88bf47959fa9472d1652c80a04e589ec45
sophbradley <111533096+sophbradley@users.noreply.github.com>
2022-11-30 18:55:33 +0000

    Added moment and moment timezone, added JavaScript to insert current time in 2 cities onload and update every second

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,24 @@
+function updateCityTime() {
+  let mexicoCityElement = document.querySelector("#mexico-city");
+  let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+  let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+  let mexicoCityTime = moment().tz("America/Mexico_City");
 
+  mexicoCityDateElement.innerHTML = mexicoCityTime.format("dddd DD MMMM YYYY");
+  mexicoCityTimeElement.innerHTML = mexicoCityTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  let seoulCityElement = document.querySelector("#seoul");
+  let seoulDateElement = seoulCityElement.querySelector(".date");
+  let seoulTimeElement = seoulCityElement.querySelector(".time");
+  let seoulCityTime = moment().tz("Asia/Seoul");
+
+  seoulDateElement.innerHTML = seoulCityTime.format("dddd DD MMMM YYYY");
+  seoulTimeElement.innerHTML = seoulCityTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateCityTime();
+setInterval(updateCityTime, 1000);
