eb600b6c4c1f4aa4e7072e0fa82020499dfa6432
sophbradley <111533096+sophbradley@users.noreply.github.com>
2022-12-01 20:04:27 +0000

    Changed a city, added an option for user's current location

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -28,6 +28,9 @@ function updateCityTime() {
 function updateCity(event) {
   setInterval(function () {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
